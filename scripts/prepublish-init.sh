19aae9330406d5167cfd6dd5b75972eca7290372
Daniel Rojas <drojas@zerobias.com>
2026-03-16 11:32:14 -0600

    fix: pin platform-content to 1.0.70 (last version with content-full.sql)

--- scripts/prepublish-init.sh
+++ scripts/prepublish-init.sh
@@ -42,7 +42,7 @@ psql -d $SU_DB -c 'CREATE ROLE "00000000-0000-0000-0000-000000000000"' || echo "
 
 TMPDIR=$(mktemp -d)
 cd $TMPDIR
-npm pack $CONTENT_PACKAGE@latest --loglevel=error --silent
+npm pack $CONTENT_PACKAGE@1.0.70 --loglevel=error --silent
 if ls zerobias-com-platform-content* 1> /dev/null 2>&1; then
   echo -e "npm pack succedded on latest content"
 else
